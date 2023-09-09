70b491b29d55408666b5c89bf326e8c9b4864ef7
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-09-09 13:47:13 +0200

    First rectangle with element buffer objects (ebo)

--- basics.cu
+++ basics.cu
@@ -13,9 +13,16 @@ void process_input(GLFWwindow *window) {
 }
 
 GLfloat vertices[] = {
-    -0.6f, -0.6f, 0.0f,
-    0.6f, -0.6f, 0.0f,
-    0.0f,  0.6f, 0.0f
+    // first triangle
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left
+};
+
+GLuint indices[] = {
+    0, 2, 3, // top left triangle
+    0, 1, 2 // bottom right triangle
 };
 
 const char* vertexShaderSource = "#version 330 core\n"
@@ -32,11 +39,9 @@ const char* fragmentShaderSource = "#version 330 core\n"
 
 
 int main() {
-    fprintf(stdout, "HELLO WORLD");
-
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800U, 600U, "opengl", NULL, NULL);
@@ -94,15 +99,30 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    GLuint vao, vbo;
+    GLuint vao, vbo, ebo;
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
 
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // Draw wireframe mode
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
 
     while(!glfwWindowShouldClose(window)) {
         // input
@@ -116,13 +136,17 @@ int main() {
         // draw
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // swap and poll events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(shaderProgram);
     glfwTerminate();
 
     return 0;
