d44e9f1f229a435c75482bcb75c5e79baf7722fe
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-09-13 20:15:11 +0200

    Add cluster constants

--- src/constants.h
+++ src/constants.h
@@ -1,24 +1,37 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define PARTICLES_CAPACITY 1024*8
-#define MAX_PARTICLES_PER_CELL 32
-#define TILE_SIZE 50
-
 #define RNG_SEED 1234
 
-#define TRANS_COEFF 0.01f
-#define ROT_COEFF 0.01f
+// Tunnels
+/* #define PARTICLES_CAPACITY 1024*4 */
+/* #define MAX_PARTICLES_PER_CELL 128 */
+/* #define TILE_SIZE 25 */
+/* #define TRANS_COEFF 1.5f */
+/* #define ROT_COEFF 1.5f */
+/* #define DELTA_T 0.01f */
+/* #define INTR_CUTOFF 25.0f */
+/* #define ATTR_STRENGTH 20.0f */
+/* #define ACTIVE_FRACTION 0.05f */
+/* #define PARTICLE_SPEED 200.0f */
+/* #define PARTICLE_RADIUS 5.0f */
+
+// Clusters
+#define PARTICLES_CAPACITY 512
+#define MAX_PARTICLES_PER_CELL 128
+#define TILE_SIZE 25
+#define TRANS_COEFF 0.22f
+#define ROT_COEFF 0.16f
 #define DELTA_T 0.01f
 #define INTR_CUTOFF 25.0f
-#define ATTR_STRENGTH 20.0f
-#define ACTIVE_FRACTION 0.2f
-#define PARTICLE_SPEED 100.0f
-#define PARTICLE_RADIUS 5.0f
+#define ATTR_STRENGTH 150.0f
+#define ACTIVE_FRACTION 1.0f
+#define PARTICLE_SPEED 40.0f
+#define PARTICLE_RADIUS 4.0f
 
 #define LOOP_AROUND
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 #endif /* CONSTANTS_H */
