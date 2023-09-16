afcc85c3c4cdebd4550240de80a3f85ff031405e
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-09-16 21:05:19 +0200

    Use paper parameters

--- src/constants.h
+++ src/constants.h
@@ -3,31 +3,53 @@
 
 #define RNG_SEED 1234
 
+// mu m
+/* #define PARTICLE_RADIUS 1.0f */
+
+// From the paper:
+// unit mu m
+// #define PARTICLE_RADIUS 1.0f
+
+// unit J*K^-1
+//   #define BOLTZMANN_CONSTANT 1.380649 * pow(10.0, -23.0)
+
+// unit K
+/* #define TEMPERATURE_KELVIN 300.0 */
+
+// unit Ns/m^2 (water)
+/* #define FLUID_VISCOSITY 0.001 */
+
+// unit mu m^2/s
+/* #define TRANS_COEFF 0.22f */
+
+// unit rad^2/s
+/* #define ROT_COEFF 0.16f */
+
 // Tunnels
-#define PARTICLES_CAPACITY 1024*16
-#define MAX_PARTICLES_PER_CELL 128
-#define TILE_SIZE 32
-#define TRANS_COEFF 150.0f
-#define ROT_COEFF 15.0f
-#define DELTA_T 0.01f
-#define INTR_CUTOFF 30.0f
-#define ATTR_STRENGTH 25.0f
-#define ACTIVE_FRACTION 0.1f
-#define PARTICLE_SPEED 100.0f
-#define PARTICLE_RADIUS 4.0f
+/* #define PARTICLES_CAPACITY 1024*16 */
+/* #define MAX_PARTICLES_PER_CELL 128 */
+/* #define TILE_SIZE 32 */
+/* #define PARTICLE_RADIUS 4.0f */
+/* #define TRANS_COEFF 0.22f / PARTICLE_RADIUS */
+/* #define ROT_COEFF 0.16f / powf(PARTICLE_RADIUS, 3) */
+/* #define DELTA_T 0.01f */
+/* #define INTR_CUTOFF 30.0f */
+/* #define ATTR_STRENGTH 25.0f */
+/* #define ACTIVE_FRACTION 0.1f */
+/* #define PARTICLE_SPEED 100.0f */
 
 // Clusters
-/* #define PARTICLES_CAPACITY 1024 */
-/* #define MAX_PARTICLES_PER_CELL 32 */
-/* #define TRANS_COEFF 22.0f */
-/* #define ROT_COEFF 16.0f */
-/* #define DELTA_T 0.01f */
-/* #define INTR_CUTOFF 10.0f */
-/* #define ATTR_STRENGTH 150.0f */
-/* #define ACTIVE_FRACTION 1.0f */
-/* #define PARTICLE_SPEED 40.0f */
-/* #define PARTICLE_RADIUS 4.0f */
-/* #define TILE_SIZE 32 */
+#define PARTICLES_CAPACITY 1024
+#define MAX_PARTICLES_PER_CELL 32
+#define TILE_SIZE 32
+#define PARTICLE_RADIUS 4.0f
+#define TRANS_COEFF 0.22f / PARTICLE_RADIUS
+#define ROT_COEFF 0.16f / powf(PARTICLE_RADIUS, 3)
+#define DELTA_T 0.01f
+#define INTR_CUTOFF 10.0f
+#define ATTR_STRENGTH 150.0f
+#define ACTIVE_FRACTION 1.0f
+#define PARTICLE_SPEED 40.0f
 
 #define LOOP_AROUND
 
